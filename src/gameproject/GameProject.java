65bae7ee942ef1950fb41bc765325e3b55b2e0f2
georgeblack <g30.l3g3nd@gmail.com>
2018-11-25 15:20:30 +0400

    Basic game loop start

--- src/gameproject/GameProject.java
+++ src/gameproject/GameProject.java
@@ -5,14 +5,18 @@
  */
 package gameproject;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -21,15 +25,45 @@ import javafx.stage.Stage;
  */
 public class GameProject extends Application {
     
+    
+    int x = 20;
+    int y = 20;
+    
     @Override
     public void start(Stage stage) throws Exception {
        Parent root = FXMLLoader.load(getClass().getResource("FXML_test.fxml"));
     
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 600, 400);
     
         stage.setTitle("FXML Welcome");
         stage.setScene(scene);
         stage.show();
+        
+        Canvas canvas = (Canvas) scene.lookup("#canvas");
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
+        
+        
+         AnimationTimer animator = new AnimationTimer(){
+
+                @Override
+                public void handle(long arg0) {
+
+                    // UPDATE
+                    x++;
+                    y++;
+
+                    // RENDER
+                    gc.clearRect(0, 0, 600, 400);
+                    gc.setFill(Color.BLACK);
+                    gc.fillRect(x, y, 30, 30);
+                }      
+            };
+
+            animator.start();     
+        
+        
+        
     }
 
     /**
